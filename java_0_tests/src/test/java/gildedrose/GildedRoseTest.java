04479ba8c2a2c13d031972a85f092d31c4db96cd
Paul Moore <pdmoore@gmail.com>
2023-09-20 09:52:32 -0700

    java example, no tests, 0% coverage to start

---
+++ java_0_tests/src/test/java/gildedrose/GildedRoseTest.java
@@ -0,0 +1,45 @@
+package gildedrose;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GildedRoseTest {
+
+    public static final String BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
+
+    @Test
+    @Disabled
+    void AgedBrie_QualityIncreases_EvenAfterSellInDate() {
+        GildedRose sut = new GildedRose(createItemArray("Aged Brie", -1, 20));
+
+        sut.updateQuality();
+
+        assertEquals(22, sut.items[0].quality, "Aged Brie improves twice as fast after sellIn date (BUG?)");
+    }
+
+    @Test
+    @Disabled
+    void BackStagePass_QualityDropsToZeroWhenConcertPasses() {
+        GildedRose sut = new GildedRose(createItemArray(BACKSTAGE_PASS, 0, 40));
+
+        sut.updateQuality();
+
+        assertEquals(0, sut.items[0].quality, "Backstage Pass is worthless when concert has passed");
+    }
+
+    @Test
+    @Disabled
+    void GenericItem_QualityDecreasesTwiceAsFastAfterSellInDate() {
+        GildedRose sut = new GildedRose(createItemArray("generic item", 0, 10));
+
+        sut.updateQuality();
+
+        assertEquals(8, sut.items[0].quality, "When sellIn date is 0 then quality decreases twice as fast");
+    }
+
+    private Item[] createItemArray(String itemName, int sellIn, int quality) {
+        return new Item[] { new Item(itemName, sellIn, quality) };
+    }
+}
