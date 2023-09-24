73db54daa355c6f62a29f5426fff7c1f23fbc1cb
Paul Moore <pdmoore@gmail.com>
2023-09-24 16:13:40 -0700

    python example with the 3 minimal tests

---
+++ python/README.md
@@ -0,0 +1,14 @@
+### Running Tests
+
+To run the tests
+
+```sh
+pytest
+```
+
+OR
+
+```sh
+python3 -m unittest test_gilded_rose.py
+```
+
---
+++ python/test_gilded_rose.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from gilded_rose import Item, GildedRose
+
+
+class GildedRoseTest(unittest.TestCase):
+
+    def test_generic_item_quality_decreases_twice_as_fast_after_sell_by(self):
+        items = [Item("generic item", 0, 10)]
+        sut = GildedRose(items)
+
+        sut.update_quality()
+
+        self.assertEqual(8, items[0].quality)
+
+    def test_brie_quality_increases_twice_as_fast_after_sell_by(self):
+        items = [Item("Aged Brie", -1, 20)]
+        sut = GildedRose(items)
+
+        sut.update_quality()
+
+        self.assertEqual(22, items[0].quality)
+
+    def test_backstage_pass_quality_drops_to_zero_when_concert_has_passed(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 40)]
+        sut = GildedRose(items)
+
+        sut.update_quality()
+
+        self.assertEqual(0, items[0].quality)
+
+if __name__ == '__main__':
+    unittest.main()
