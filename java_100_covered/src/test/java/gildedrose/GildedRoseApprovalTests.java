2c345edab2512ecf134ea55f3e35f7faf98ccbed
Paul Moore <pdmoore@gmail.com>
2023-09-25 18:21:25 -0700

    java-100 align comments

--- java_100_covered/src/test/java/gildedrose/GildedRoseApprovalTests.java
+++ java_100_covered/src/test/java/gildedrose/GildedRoseApprovalTests.java
@@ -43,8 +43,8 @@ public class GildedRoseApprovalTests {
         CombinationApprovals.verifyAllCombinations(
                 this::doUpdateQuality,
                 new String[] {"foo", "Aged Brie", BACKSTAGE_PASS, LEGENDARY},
-                new Integer[] {-1, 0, 1, 2, 3, 4, 5, 6, 9, 10, 11},    // SellIn
-                new Integer[] { 0, 1, 5, 10, 49, 50 }       // Quality
+                new Integer[] {-1, 0, 1, 2, 3, 4, 5, 6, 9, 10, 11},  // SellIn
+                new Integer[] { 0, 1, 5, 10, 49, 50 }                // Quality
         );
     }
 
